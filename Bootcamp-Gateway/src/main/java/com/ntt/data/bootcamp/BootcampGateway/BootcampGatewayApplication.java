d94f7927e98aaad8117d8120ef64f05dc11e43f0
Marina Valencia Moreno <mvalenmo@emeal.nttdata.com>
2022-10-26 17:14:56 +0200

    primer mensaje

---
+++ Bootcamp-Gateway/src/main/java/com/ntt/data/bootcamp/BootcampGateway/BootcampGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.data.bootcamp.BootcampGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampGatewayApplication.class, args);
+	}
+
+}
---
+++ Bootcamp-Gateway/src/test/java/com/ntt/data/bootcamp/BootcampGateway/BootcampGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.data.bootcamp.BootcampGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
